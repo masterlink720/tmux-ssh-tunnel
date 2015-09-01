332282a399b2fb50bf8f20812c206da2817539d5
Brandon Simmons <brandon@binarybeast.com>
2015-09-01 06:17:34 -0700

    Client installer reformat

--- client/install.sh
+++ client/install.sh
@@ -40,28 +40,8 @@ if ! grep -q ${tunnel_user} /etc/passwd; then
 fi
 
 # Log in as the tunnel user
-su - ${tunnel_user}
+sudo -n -E -u ${tunnel_user} bash -c < ./.tunneler_install.sh
 
-# Setup ssh dir
-mkdir ~/.ssh && \
-	chmod 600 ~/.ssh
-
-# Generate an ssh key
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
-
-# Setup cron
-echo -e "\n\n\n#Start the ssh tunnel on each system boot" \
-	"\n@reboot tmux-session -d -s ssh_tunnel \"${tunnel_dir}/tunnel.sh\"" \
-	"\n" > ~/.cron
-
-crontab ~/.cron && \
-	rm ~/.cron
-
-# Deploy the key
-echo -e "\n\n\t - We're not going to deploy the SSH key to th eserver, hopefully you've run the server installation first" \
-	"\n\tIf not, do so now and keep note of the password it gives you\n\n"
-
-ssh-copy-id ${remote_server} -p ${remote_port}
 
 # Creating an ssh key to allow automated connections
 # sudo -n -E -u ${tunnel_user} sh -c "ssh-keygen -q -t rsa -N '' -f /home/${tunnel_user}/.ssh/id_rsa"
