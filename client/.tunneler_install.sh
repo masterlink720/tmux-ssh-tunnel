fb9d3258e93c6c726e434ab3804a5e44d6ce9c2c
Brandon Simmons <brandon@binarybeast.com>
2016-05-07 21:01:49 -0600

    config is now config.sh and must be copied to .config.sh before use

---
+++ client/.tunneler_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Setup ssh dir
+mkdir -p ~/.ssh && \
+	chmod 600 ~/.ssh
+
+# Generate an ssh key
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+
+# Setup cron
+echo -e "\n\n\n#Start the ssh tunnel on each system boot" \
+	"\n@reboot tmux-session -d -s ssh_tunnel \"${tunnel_dir}/tunnel.sh\"" \
+	"\n" > ~/.cron
+
+crontab ~/.cron && \
+	rm ~/.cron
+
+# Deploy the key
+echo -e "\n\n\t - We're nowsu  going to deploy the SSH key to th eserver, hopefully you've run the server installation first" \
+	"\n\tIf not, do so now and keep note of the password it gives you\n\n"
+
+ssh-copy-id ${remote_server} -p ${remote_port}
---
+++ config.sh
@@ -0,0 +1,10 @@
+# This is an example, you must copy this file to .config.sh to implement
+export tunnel_user='tunnel_user'
+export tunnel_port=2222
+export local_port=22
+export remote_port=22
+export local_forward="127.0.0.1:${local_port}"
+export remote_server='127.0.0.1'
+export tunnel_dir="/home/${tunnel_user}/ssh_tunnel"
+export log="${tunnel_dir}/tunnel.log"
+export err_log="/${tunnel_dir}/tunnel.err"
